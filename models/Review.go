dc03ee4d7dd07a5bfb2fb3c39ad02e103d7abbc2
Kalveir <71752646+Kalveir@users.noreply.github.com>
2024-12-22 16:03:11 +0700

    Update Review.go

--- models/Review.go
+++ models/Review.go
@@ -4,13 +4,13 @@ import (
 	"time"
 )
 
-type Riview struct {
-	ID        uint `gorm:"primaryKey"`
-	UserID    uint
-	User      User `gorm:"foreignKey;UserID"`
-	ProductID uint
-	Product   Product `gorm:"foreignKey;ProductID"`
-	Rating    int     `gorm:"check:rating >= 1 AND rating <= 5"`
-	Comment   string
-	CreatedAt time.Time
+type Review struct {
+	ID        uint      `gorm:"primaryKey"`
+	UserID    uint      `gorm:"not null"`
+	User      User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	ProductID uint      `gorm:"not null"`
+	Product   Product   `gorm:"foreignKey:ProductID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Rating    int       `gorm:"not null;check:rating >= 1 AND rating <= 5"`
+	Comment   string    `gorm:"type:text"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
 }
