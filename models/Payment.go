3137623259aade18f7f0b7ca1d489c60b405ec5f
Kalveir <71752646+Kalveir@users.noreply.github.com>
2024-12-22 16:02:17 +0700

    Update Payment.go

--- models/Payment.go
+++ models/Payment.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Payment struct {
-	ID            uint `gorm:"primaryKey"`
-	OrderID       uint
-	Order         Order     `gorm:"foreignKey:OrderID"`
+	ID            uint      `gorm:"primaryKey"`
+	OrderID       uint      `gorm:"not null"`
+	Order         Order     `gorm:"foreignKey:OrderID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Amount        float64   `gorm:"not null"`
 	PaymentDate   time.Time `gorm:"default:CURRENT_TIMESTAMP"`
-	PaymentMethod string    `gorm:"type:enum('bank_transfer','qris');not null"`
-	Status        string    `gorm:"type:enum('pending','complated','failed');default:'pending'"`
+	PaymentMethod string    `gorm:"type:varchar(20);not null;check:payment_method IN ('bank_transfer', 'qris')"`
+	Status        string    `gorm:"type:varchar(20);default:'pending';check:status IN ('pending', 'completed', 'failed')"`
 }
