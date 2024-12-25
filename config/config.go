878fef006e8a007c996bf0b631c1ce5be1c575c7
Kalveir <71752646+Kalveir@users.noreply.github.com>
2024-12-25 20:34:27 +0700

    Update : config.go

--- config/config.go
+++ config/config.go
@@ -1,14 +1,18 @@
 package config
 
 import (
+	"log"
 	"time"
-
+        "os"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/cors"
 	// "github.com/gofiber/fiber/v3/middleware/csrf"
 	"github.com/gofiber/fiber/v3/middleware/limiter"
 	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/fiber/v3/middleware/requestid"
 	"github.com/gofiber/fiber/v3/middleware/recover"
+	"github.com/gofiber/fiber/v3/middleware/idempotency"
+	"github.com/gofiber/fiber/v3/middleware/helmet"
 )
 
 // FiberConfig berisi konfigurasi Fiber yang aman
@@ -17,6 +21,7 @@ func FiberConfig() fiber.Config {
 		AppName:       "fiber-api",
 		CaseSensitive: true,
 		StrictRouting: true,
+		EnablePrefork : true,
 		ServerHeader:  "Fiber",
 	}
 }
@@ -27,13 +32,28 @@ func MiddlewareConfig(app *fiber.App) {
 	app.Use(recover.New())
 
 	// Logging
-	app.Use(logger.New())
+	app.Use(requestid.New())
+	logFile, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("Failed to open log file: " + err.Error())
+	}
+	defer logFile.Close()
+	app.Use(logger.New(logger.Config{
+			Format: "${locals:requestid} ${ip} ${status} - ${method} ${path} ${body}\n",
+			Output: logFile,
+	}))
 
 	// Rate Limiting
 	app.Use(limiter.New(limiter.Config{
 		Max:        20,
 		Expiration: 30 * time.Second,
 	}))
+	
+	//Helmet
+	app.Use(helmet.New())
+	
+	//Idempotency
+	app.Use(idempotency.New())
 
 	// CSRF Protection
 	// app.Use(csrf.New())
