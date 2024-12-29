de50c02fe5ce23e154b468f253f00584bdec585f
Kalveir <71752646+Kalveir@users.noreply.github.com>
2024-12-29 19:45:54 +0700

    Update: Review.go

--- models/Review.go
+++ models/Review.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Review struct {
-	ID        uint      `gorm:"primaryKey"`
-	UserID    uint      `gorm:"not null"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	UserID    uint      `json: "user_id" gorm:"not null"`
 	User      User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	ProductID uint      `gorm:"not null"`
+	ProductID uint      `json:"product_id" gorm:"not null"`
 	Product   Product   `gorm:"foreignKey:ProductID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Rating    int       `gorm:"not null;check:rating >= 1 AND rating <= 5"`
-	Comment   string    `gorm:"type:text"`
+	Rating    int       `json:"rating" gorm:"not null;check:rating >= 1 AND rating <= 5"`
+	Comment   string    `json:"comment" gorm:"type:text"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 }
