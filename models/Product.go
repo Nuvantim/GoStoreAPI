f42a20c6185440ce158ebcc178d434daf355076d
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-01-01 08:32:16 +0700

    Update Product.go

--- models/Product.go
+++ models/Product.go
@@ -1,10 +1,15 @@
 package models
+import(
+	"gorm.io/gorm"
+)
 
 type Product struct {
+	gorm.Model
 	ID          uint     `json:"id" gorm:"PrimaryKey;autoIncrement"`
 	Name        string   `json:"name" gorm:"not null"`
 	Description string   `json:"description" gorm:"not null"`
 	Price       uint     `json:"price" gorm:"not null"`
 	Stock       uint     `json:"stock" gorm:"default:0"`
 	CategoryID  uint     `json:"category_id"`
+	Category    Category
 }
