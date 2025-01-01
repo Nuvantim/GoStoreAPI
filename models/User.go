d9f387e3746e311021448ab86a7eef1eed6ba8db
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-01-01 17:25:28 +0700

    Create: model user_info

--- models/User.go
+++ models/User.go
@@ -10,7 +10,18 @@ type User struct {
 	Name     	string 		`json:"name" gorm:"not null"`
 	Email    	string 		`json:"email" gorm:"not null"`
 	Password 	string 		`json:"password" gorm:"not null"`
-	Address  	string 		`json:"address" gorm:"not null"`
-	Phone    	uint   		`json:"phone" gorm:"not null"`
-	CreatedAt   time.Time 	`gorm:"type:date"`
+	CreatedAt       time.Time 	`gorm:"autoCreateTime"`
+}
+
+type user_info struct {
+	ID              uint            `json:"id" gorm:"PrimaryKey;autoIncrement"`
+	UserID          uint            `json:"user_id"`
+	Age             uint            `json:"age"`
+	Phone    	uint   		`json:"phone"`
+	District        string          `json:"district"`
+	City            string          `json:"city"`
+	State           string          `json:"state"`
+	Country         string          `json:"country"`
+	CreatedAt       time.Time       `gorm:"autoCreateTime"`
+	  
 }
