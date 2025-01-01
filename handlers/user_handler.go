d2fa72c1aa6dbb29241d03ad7d7fd74bf4278e3e
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-01-01 17:29:20 +0700

    Update user_handler.go

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -12,8 +12,6 @@ var user struct {
 	Name     string `json:"name" validate:"required"`
 	Email    string `json:"email" validate:"required"`
 	Password string `json:"password" validate:"required"`
-	Address  string `json:"address" validate:"required"`
-	Phone    uint   `json:"phone" validate:"required"`
 }
 
 func GetProfile(c fiber.Ctx) error {
