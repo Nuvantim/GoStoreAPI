6b55798b2859b8976aaa718054c8e1bc1840cc16
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-01-02 15:38:19 +0700

    Update user_service.go

--- service/user_service.go
+++ service/user_service.go
@@ -34,7 +34,6 @@ func RegisterUser(users models.User) map[string]interface{} {
 }
 
 
-
 func GetUser() []models.User {
 	var user []models.User
 	database.DB.Find(&user)
@@ -61,22 +60,21 @@ func FindUser(id uint) map[string]interface{} {
 	return data
 }
 
-func UpdateUser(req map[string]interface{}, id uint) map[string]interface{} {
+func UpdateUser(users models.User, user_info models.UserInfo, id uint) map[string]interface{} {
 	// Ambil data user berdasarkan id
 	var user models.User
 	if err := database.DB.First(&user, id).Error; err != nil {
 		return map[string]interface{}{"error": "User not found"}
 	}
 
-	// Update user
-	user.Name = req["name"].(string)
-	user.Email = req["email"].(string)
-	// Hash password
-	if password, ok := req["password"].(string); ok && password != "" {
-		hash := utils.HashBycrypt(password)
+	// update user
+	user.Name = users.Name
+	user.Email = users.Email
+	if Password != nil {
+		hash := utils.HashBycrypt(users.Password)
 		user.Password = string(hash)
 	}
-
+	
 	// Simpan perubahan user
 	if err := database.DB.Save(&user).Error; err != nil {
 		return map[string]interface{}{"error": "Failed to update user"}
@@ -89,12 +87,12 @@ func UpdateUser(req map[string]interface{}, id uint) map[string]interface{} {
 	}
 
 	// Update user_info
-	userInfo.Age = req["age"].(uint)
-	userInfo.Phone = req["phone"].(uint)
-	userInfo.District = req["district"].(string)
-	userInfo.City = req["city"].(string)
-	userInfo.State = req["state"].(string)
-	userInfo.Country = req["country"].(string)
+	userInfo.Age = user_info.Age
+	userInfo.Phone = user_info.Phone
+	userInfo.District = user_info.District
+	userInfo.City = user_info.City
+	userInfo.State = user_info.State
+	userInfo.Country = user_info.Country
 
 	// Simpan perubahan user_info
 	if err := database.DB.Save(&userInfo).Error; err != nil {
@@ -109,8 +107,6 @@ func UpdateUser(req map[string]interface{}, id uint) map[string]interface{} {
 
 	return data
 }
-
-
 
 func DeleteUser(id string) error {
 	var user models.User
