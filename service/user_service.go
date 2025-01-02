a7e633e67c57755f0b57c7baa4d59954abcc3373
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-01-02 14:39:43 +0700

    Update user_service.go

--- service/user_service.go
+++ service/user_service.go
@@ -73,7 +73,7 @@ func UpdateUser(req map[string]interface{}, id string) map[string]interface{} {
 	user.Email = req["email"].(string)
 	// Hash password
 	if password, ok := req["password"].(string); ok && password != "" {
-		hash, err := utils.Bcrypt(password)
+		hash, err := utils.bcrypt(password)
 		if err != nil {
 			return map[string]interface{}{"error": "Failed to hash password"}
 		}
