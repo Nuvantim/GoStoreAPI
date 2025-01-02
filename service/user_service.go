06de18a20da3fe9454a64e254722259ce59e48ec
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-01-02 14:44:13 +0700

    Update user_service.go

--- service/user_service.go
+++ service/user_service.go
@@ -73,7 +73,7 @@ func UpdateUser(req map[string]interface{}, id string) map[string]interface{} {
 	user.Email = req["email"].(string)
 	// Hash password
 	if password, ok := req["password"].(string); ok && password != "" {
-		hash,_ := utils.HashBycrypt(password)
+		hash := utils.HashBycrypt(password)
 		user.Password = hash
 	}
 
