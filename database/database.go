6c2e071fffe507bf685cb6a3138e10936b1afd3e
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-01-06 18:24:34 +0700

    Update database.go

--- database/database.go
+++ database/database.go
@@ -8,20 +8,22 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"os"
+	"time"
 )
 
 var (
 	DB *gorm.DB
 )
 
-// connectDb
+// MysqlConnect initializes the connection to the MySQL database
 func MysqlConnect() {
+	// Load environment variables
 	err := godotenv.Load()
-
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Prepare the DSN (Data Source Name)
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
@@ -29,23 +31,44 @@ func MysqlConnect() {
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"))
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+	// Open the MySQL database connection using GORM
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,  // Avoid default transaction wrapping
+		PrepareStmt:            true,  // Enable statement preparation for better performance
+	})
 	if err != nil {
-		log.Fatal("Failed to connect to database. \n", err)
+		log.Fatal("Failed to connect to database: ", err)
 		os.Exit(2)
 	}
 
-	log.Println("connected")
-	db.AutoMigrate(&models.User{})
-	db.AutoMigrate(&models.Product{})
-	db.AutoMigrate(&models.Category{})
-	db.AutoMigrate(&models.Order{})
-	db.AutoMigrate(&models.OrderItem{})
-	db.AutoMigrate(&models.Cart{})
-	db.AutoMigrate(&models.UserInfo{})
-	// db.AutoMigrate(&models.Paytment{})
-	db.AutoMigrate(&models.Review{})
-	DB = db
+	// Set up database connection pooling
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get db object: ", err)
+	}
+	// Set maximum idle and open connections, and maximum connection lifetime
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(30 * time.Minute)
 
+	// Perform AutoMigrate only for necessary models (or in development environment)
+	log.Println("Connected to the database successfully")
+
+	// AutoMigrate for models that need database schema changes
+	err = db.AutoMigrate(
+		&models.User{},
+		&models.Product{},
+		&models.Category{},
+		&models.Order{},
+		&models.OrderItem{},
+		&models.Cart{},
+		&models.UserInfo{},
+		&models.Review{},
+	)
+	if err != nil {
+		log.Fatal("AutoMigrate failed: ", err)
+	}
+
+	// Assign the DB object to the global variable
+	DB = db
 }
