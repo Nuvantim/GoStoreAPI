894f39aeb2b89869c8a1356665c146d18413015b
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-01-12 19:52:42 +0700

    Update Product.go

--- models/Product.go
+++ models/Product.go
@@ -7,11 +7,11 @@ import (
 type Product struct {
 	ID          uint      `json:"id" gorm:"PrimaryKey;autoIncrement"`
 	Name        string    `json:"name" gorm:"not null"`
-	Description string    `json:"description" gorm:"not null"`
+	Description string    `json:"description, omitempty" gorm:"not null"`
 	Price       uint      `json:"price" gorm:"not null"`
 	Stock       uint      `json:"stock" gorm:"default:0"`
-	CategoryID  uint      `json:"category_id"`
-	Category    *Category  `gorm:"foreignKey:CategoryID"`
-	Review      *[]Review  `json:"review"`
+	CategoryID  uint      `json:"category_id" gorm:"not null"`
+	Category    Category  `json:"category, omitempty" gorm:"foreignKey:CategoryID"`
+	Review      []Review  `json:"review,omitempty"`
 	CreatedAt   time.Time `gorm:"autoCreateTime"`
 }
