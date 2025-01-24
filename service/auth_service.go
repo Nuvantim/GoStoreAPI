58525d6820a2f54dfea4ca8070761ebddf3bbc84
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-01-24 15:05:58 +0700

    Update auth_service.go

--- service/auth_service.go
+++ service/auth_service.go
@@ -40,33 +40,75 @@ func Login(email, password string) (string, string, error) {
 	return accessToken, refreshToken, nil
 }
 
-func OtpVerify(otp string) map[string]interface{}{
-	// check otp
-	var usertemp models.UserTemp
-	database.DB.Where("otp = ?", otp).Take(&usertemp)
-	// Buat user baru
-	user := models.User{
-		Name:     usertemp.Name,
-		Email:    usertemp.Email,
-		Password: usertemp.Password,
-	}
-	// Simpan user ke database
-	database.DB.Create(&user)
+func OtpVerify(otp string) map[string]interface{} {
+    // Validasi input OTP
+    if otp == "" {
+        return map[string]interface{}{
+            "error": "OTP tidak boleh kosong",
+        }
+    }
 
-	// Buat info user
-	info := models.UserInfo{
-		UserID: user.ID,
-	}
-	// Simpan info user ke database
-	database.DB.Create(&info)
+    // Cari user temporary
+    var userTemp models.UserTemp
+    result := database.DB.Where("otp = ?", otp).First(&userTemp)
+    if result.Error != nil {
+        return map[string]interface{}{
+            "error": "OTP tidak valid",
+        }
+    }
 
-	// delete userTemp
-	var user_temp models.UserTemp
-	database.DB.First(&user_temp, usertemp.ID)
-	database.DB.Delete(&user_temp)
+    // Mulai transaksi database
+    tx := database.DB.Begin()
+    if tx.Error != nil {
+        return map[string]interface{}{
+            "error": "Gagal memulai transaksi database",
+        }
+    }
 
-	alert := map[string]interface{}{
-		"message": "Success Verification, Please Login !",
-	}
-	return alert
+    // Buat user baru
+    user := models.User{
+        Name:     userTemp.Name,
+        Email:    userTemp.Email,
+        Password: userTemp.Password,
+    }
+
+    // Simpan user ke database
+    if err := tx.Create(&user).Error; err != nil {
+        tx.Rollback()
+        return map[string]interface{}{
+            "error": "Gagal membuat user",
+        }
+    }
+
+    // Buat info user
+    userInfo := models.UserInfo{
+        UserID: user.ID,
+    }
+
+    // Simpan info user ke database
+    if err := tx.Create(&userInfo).Error; err != nil {
+        tx.Rollback()
+        return map[string]interface{}{
+            "error": "Gagal membuat info user",
+        }
+    }
+
+    // Hapus user temporary
+    if err := tx.Delete(&userTemp).Error; err != nil {
+        tx.Rollback()
+        return map[string]interface{}{
+            "error": "Gagal menghapus user temporary",
+        }
+    }
+
+    // Commit transaksi
+    if err := tx.Commit().Error; err != nil {
+        return map[string]interface{}{
+            "error": "Gagal commit transaksi",
+        }
+    }
+
+    return map[string]interface{}{
+        "message": "Verifikasi berhasil, silakan login!",
+    }
 }
